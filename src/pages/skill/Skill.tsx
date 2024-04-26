732f3d57f776e1e629dcf0f0650d7efb769eb598
Bryan <brianforonda94@gmail.com>
2024-04-26 00:58:18 -0500

    responsibe

--- src/pages/skill/Skill.tsx
+++ src/pages/skill/Skill.tsx
@@ -1,32 +1,33 @@
 import { DataSkill } from '../../models/DataSkill';
 import { SkillItem } from '../../models/Skill';
+import { Section } from '../../styled-components/skill/Skill';
 
 export const Skill = () => {
   return (
-    <section className='container-section animate__animated animate__fadeIn' style={{width: "100%", padding: "0px 20px 20px 20px"}}>
-      <header style={{marginBottom: "30px"}}>
-        <h1 style={{fontSize: "3rem", lineHeight: "1" }}>Habilidades.</h1>
+    <Section className='container-section animate__animated animate__fadeIn contain__skill'>
+      <header>
+        <h1>Habilidades.</h1>
       </header>
       <footer>
-        <section style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "4vw", width: "100%", justifyItems: "center", alignItems: "center" }}>
+        <section className='contain__skill--grid'>
           {
             DataSkill.map(( skill: SkillItem ) => (
-              <article className='grow  p-2' style={{borderRadius: "15px", background: "rgb(46 53 65 / 10%)", width: "14vw"}}>
+              <article className='grow contain__skill--grid-box p-2'>
                 <span className='d-flex justify-content-center align-items-center flex-column'>
                   {
                     (skill.name === 'Styled-Components' || skill.name === 'TypeScript')
                     ? (typeof skill.logo === 'string' 
-                       ? <img src={skill.logo} style={{height: "80px"}} alt={skill.name} />
+                       ? <img src={skill.logo} className="contain__skill--grid-img" alt={skill.name} />
                        : skill.logo)
                     : skill.logo
                   }
-                  <p style={{fontWeight: "bold"}}>{skill.name}</p>
+                  <p className="contain__skill--grid-title">{skill.name}</p>
                 </span>
               </article>
             ))
           }
         </section>
       </footer>
-    </section>
+    </Section>
   )
 }
---
+++ src/styled-components/skill/Skill.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  .contain__skill {
+    width: 100%;
+    padding: 0px 20px 20px;
+  }
+
+  header {
+    margin-bottom: 30px;
+  }
+
+  header h1 {
+    font-size: 3rem;
+    line-height: 1;
+  }
+
+  .contain__skill--grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 4vw;
+    width: 100%;
+    place-items: center;
+  }
+
+  .contain__skill--grid-box {
+    border-radius: 15px;
+    background: rgba(46, 53, 65, 0.1);
+    width: 14vw;
+  }
+
+  .contain__skill--grid-img {
+    height: 80px;
+  }
+
+  .contain__skill--grid-title {
+    font-weight: bold;
+  }
+
+  @media (max-width: 640px) {
+    .contain__skill--grid {
+      grid-template-columns: repeat(1, 1fr);
+    }
+
+    .contain__skill--grid-box {
+      width: 60vw;
+    }
+
+    .contain__skill--grid-box:last-child {
+      margin-bottom: 20px;
+    }
+  }
+`;
