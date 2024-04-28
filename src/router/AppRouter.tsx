f3fbb8dd0058c14416049a8782ff19ca1bfaf6fa
Bryan <brianforonda94@gmail.com>
2024-04-28 17:50:18 -0500

    responsibe

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { AboutMe } from '../pages/aboutMe/AboutMe'
 import Navbar from '../components/Navbar'
 import { Project } from '../pages/project/Project'
